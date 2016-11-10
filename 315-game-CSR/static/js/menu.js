88dab23470d4eb720e57886e13ff60ab7fb04b04
DTAAYGUN <ahmetfaruk.aygun@digiturk.com.tr>
2016-11-10 15:14:19 +0300

    Revert "Son sürüm"
    This reverts commit 29558e97227a75c1af0e3b6d6416ae83c5691d6c.

--- 315-game-CSR/static/js/menu.js
+++
@@ -1,9 +0,0 @@
-//  Test functions to be replaced with things that actually do the game
-
-function select(shipType) {
-    displayMessage(shipType);
-}
-
-function action(actType) {
-    displayMessage(actType);
-}
