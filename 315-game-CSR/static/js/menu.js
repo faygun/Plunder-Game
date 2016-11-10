29558e97227a75c1af0e3b6d6416ae83c5691d6c
DTAAYGUN <ahmetfaruk.aygun@digiturk.com.tr>
2016-11-10 14:45:44 +0300

    Son sürüm
    Signed-off-by: DTAAYGUN <ahmetfaruk.aygun@digiturk.com.tr>

---
+++ 315-game-CSR/static/js/menu.js
@@ -0,0 +1,9 @@
+//  Test functions to be replaced with things that actually do the game
+
+function select(shipType) {
+    displayMessage(shipType);
+}
+
+function action(actType) {
+    displayMessage(actType);
+}
